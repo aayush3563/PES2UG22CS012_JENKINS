36d50a9a6f9d0ae2d5c89d22885fba778cff0009
aayush3563 <aayushkumar3563@gmail.com>
2025-03-11 10:25:31 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
+  cout<< "Version changed SRN is PES2UG22CS012\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
